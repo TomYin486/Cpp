e58c8f342fdf15166ca7905c2d731c8dc9141afd
jojo--1 <1468608544@qq.com>
2025-01-25 22:21:30 +0800

    C++：（使用 lower_bound）在一个特定的数组 data 中（大小为 200，data[i] = 4 * i + 6）查找一个给定整数 target（假设 target 一定在 data 中）的下标

--- Sandy/Sandy/sandy.cpp
+++ Sandy/Sandy/sandy.cpp
@@ -3,49 +3,22 @@
 #include<algorithm>
 using namespace std;
 
-const int N = 5e5 + 9;
-int nums[N];
-
-// 比较函数 cmp 用于 sort 函数中的降序排序
-bool cmp(const int& p1, const int& p2)
-{
-	// int &p1 表示 p1 是一个 int 类型的引用，可理解为 p1 是另一个 int 类型变量的别名
-	// 如果 p1 大于 p2，则返回 true，表示 p1 应该排在 p2 前面
-	return p1 > p2;
-}
-
 int main()
 {
-	int n;
-	cin >> n;
-
-	int i = 0;
-	for (i = 0; i < n; i++)
+	int data[200] = { 0 };
+	for (int i = 0; i < 200; i++)
 	{
-		cin >> nums[i];
+		// 为每个元素进行赋值，可以看出数组 data 里的元素是升序的
+		data[i] = i * 4 + 6;
 	}
 
-	// 进行升序排序，范围是[nums, num + n)
-	sort(nums, nums + n);
+	int target = 0;
+	// 假设 target 一定在数组 data 中
+	cin >> target;
 
-	for (i = 0; i < n; i++)
-	{
-		cout << nums[i] << ' ';
-	}
-	cout << '\n';
+	// 使用 lower_bound() 在数组 data 中利用二分查找第一个大于或等于 target 的元素的位置
+	// lower_bound 返回一个指向该元素的地址
+	cout << (lower_bound(data, data + 200, target) - data) << '\n';
 
-	// 降序排序
-	sort(nums, nums + n, cmp);
-
-	for (i = 0; i < n; i++)
-	{
-		cout << nums[i] << ' ';
-	}
-
-	// 方法二：逆序输出
-	/*for (i = n - 1; i >= 0; i--)
-	{
-		cout << nums[i] << ' ';
-	}*/
 	return 0;
 }
