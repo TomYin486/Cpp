420050beb477dcc767ef0b3eadce54944000cf10
jojo--1 <1468608544@qq.com>
2025-02-08 21:50:50 +0800

    C++：（3419）小郑的蓝桥平衡串，（使用前缀和），给定一个只包含字符 L 和 Q 的字符串（长度 <= 1000），要求找到其中最长的平衡子串（子串中 L 和 Q 的数量相等）

--- Sandy/Sandy/sandy.cpp
+++ Sandy/Sandy/sandy.cpp
@@ -1,57 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstring>     // 用于使用 strlen
 using namespace std;
-using ll = long long;
 
-const ll p = 1e9 + 7;      // p 为模数
-const int N = 1e5 + 9;     // N 为数组的最大大小
-ll a[6][N], prefix[6][N];  // a 数组存储不同次方的值，prefix 数组存储前缀和
+// 将 L 看做 1，Q 看做 -1，只有当某个区间的和为 0 时，字符串是平衡的
+// 可以预处理出前缀和，然后枚举所有区间（这一步的时间复杂度是 O(n ^ 2)的），得到所有平衡区间的长度最后取大输出
 
-// 由于 k 比较小，可以处理出 5 个数组分别表示不同的次方，如 a3[] 中的元素都是数组 a 中元素的 3 次方
-// 再对 5 个数组预处理出前缀和，对于每次询问利用前缀和的性质可 O(1) 解决
+const int N = 1010;   // 数组的最大大小  
+char s[N];            // 用于输入的字符串  
+int prefix[N];        // 存储前缀和数组  
 
 int main()
 {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);    // 加速输入输出
-    int n, m;         // n 和 m 分别为数组长度和查询次数
-    cin >> n >> m;
+    cin >> s + 1;            // 从下标 1 开始输入字符串
+    int n = strlen(s + 1);   // 计算字符串的长度
 
     int i = 0;
     int j = 0;
+    prefix[0] = 0;
 
-    // 输入原始数组
-    for (i = 1; i <= n; ++i)
+    // 计算前缀和数组，'L' 对应 +1，'Q' 对应 -1
+    for (i = 1; i <= n; i++)
     {
-        cin >> a[1][i];
+        prefix[i] = prefix[i - 1] + (s[i] == 'L' ? 1 : -1);
     }
 
-    // 计算每个元素的 2 到 5 的次方，并取模
-    for (i = 2; i <= 5; ++i)
+    int ans = 0;         // 表示最长平衡串的长度
+
+    // 遍历所有可能的子串，检查是否为平衡串
+    for (i = 1; i <= n; i++)
     {
-        for (j = 1; j <= n; ++j)
+        for (j = i; j <= n; j++)
         {
-            a[i][j] = a[i - 1][j] * a[1][j] % p;
+            // 如果前缀和差为 0，则子串中 'L' 和 'Q' 的数量相等
+            if (prefix[j] - prefix[i - 1] == 0)
+            {
+                ans = max(ans, j - i + 1);      // 更新最长平衡串的长度
+            }
         }
     }
 
-    // 计算每个次方的前缀和，并取模
-    for (i = 1; i <= 5; ++i)
-    {
-        for (j = 1; j <= n; ++j)
-        {
-            prefix[i][j] = (prefix[i][j - 1] + a[i][j]) % p;
-        }
+    cout << ans << '\n';      // 输出最长平衡串的长度
 
-    }
-
-    // 处理每个查询
-    while (m--)
-    {
-        int l, r, k;
-        cin >> l >> r >> k;   // 输入查询的左右边界和次方
-
-        // 输出查询结果，处理了负数情况
-        cout << (prefix[k][r] - prefix[k][l - 1] + p) % p << "\n";
-    }
     return 0;
 }
