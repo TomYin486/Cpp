77d31107c3d4ce123bc52596cefb5fedf543f0a9
jojo--1 <1468608544@qq.com>
2025-02-12 22:03:22 +0800

    C++：（1331）二进制中 1 的个数，给定一个 int 类型整数 x，输出该数二进制表示中 1 的个数

--- Sandy/Sandy/sandy.cpp
+++ Sandy/Sandy/sandy.cpp
@@ -1,41 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <vector>   
-#include<string>
-#include<algorithm>
 using namespace std;
 
-class Solution {
-public:
-    string largestNumber(vector<int>& nums) {
-        // 优化，将所有的整数转换为字符串
-        vector<string> strs;
-        for (int x : nums)
+int main()
+{
+    // int 类型，右移操作是算术右移，在左侧补符号位（正数补 0，负数补 1）
+    // unsigned int 类型，右移操作是逻辑右移，在左侧补 0
+    unsigned int n;
+    cin >> n;
+    int count = 0;   // 用于统计二进制中 1 的个数
+
+    while (n > 0)
+    {
+        // 检查 n 的最低位是否为 1
+        if ((n & 1) != 0)
         {
-            strs.push_back(to_string(x));
+            // 如果最低位是 1，计数器加 1
+            count++;
         }
-
-        // 排序
-        sort(strs.begin(), strs.end(), [](const string& s1, const string& s2)
-            {
-                // 如果 s1 + s2 > s2 + s1，则 s1 排在 s2 前面
-                return s1 + s2 > s2 + s1;
-            });
-
-        // 将排序后的字符串拼接成最终的结果
-        string ret;
-        for (auto& s : strs)
-        {
-            ret += s;
-        }
-
-        // 如果结果的第一个字符是 '0'，说明所有数字都是 0，直接返回 "0"
-        if (ret[0] == '0')
-        {
-            return "0";
-        }
-
-        // 返回拼接后的最大数
-        return ret;
+        n >>= 1;    // 将 n 右移一位，相当于去掉最低位
     }
-};
+
+    cout << count << '\n';
+    return 0;
+}
