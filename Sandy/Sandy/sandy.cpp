3cf85b0537d3a94fe809f65eeeafa48a6d887ea0
jojo--1 <1468608544@qq.com>
2025-02-19 12:11:13 +0800

    C++：（1621）挑选子串（快慢指针），从 n 个整数中选出一个连续子串且这个字串里面至少有 k 个数要 >= m，计算满足条件的连续子串的总数

--- Sandy/Sandy/sandy.cpp
+++ Sandy/Sandy/sandy.cpp
@@ -2,54 +2,46 @@
 #include <iostream>
 using namespace std;
 
-const int N = 1e5 + 9;   // 数组的最大长度
-int a[N];            
+const int N = 2e5 + 9;
+int a[N];
 
-// 利用快慢指针，保持区间 [i, j] 尽可能小且和 >= S
-// 当 [i, j] 中的 sum >= S 后，再增加 j 没有意义，所以 i, j 都不动
-// 当 i 增大后 sum 可能 < S，此时 j 不可能左移，所以只能右移，于是 i, j 都只能右移，从而出现左移
-
+// 利用快慢指针，保持区间 [i,j] 中 >= m 的个数至少有 k 个，且区间尽可能小
+// 得到区间 [i,j] 后，对于这个左端点，有 n - j + 1 个右段点可取
 int main()
 {
-    // 加速输入输出流
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0); // 优化输入输出
+    int n, m, k;
+    cin >> n >> m >> k;
 
-    int n, S;         // n 是序列长度，S 是目标区间和
-    cin >> n >> S;    
-
-    int i = 0;
-    // 读取序列中的元素
-    for (i = 1; i <= n; i++)
+    // 得到数组 a
+    for (int i = 1; i <= n; i++)
     {
         cin >> a[i];
     }
 
-    int ans = n + 1;   // ans 为 n + 1，表示一个不可能的长度，用于后面判断
-    int j = 0;
-    int sum = 0;
+    int ans = 0;    // 表示满足条件的子串数量
 
-    // 双指针
-    for (i = 1; i <= n; i++)
+    // 使用双指针 i 和 j 来遍历数组
+    // cnt 用于记录当前子串中大于等于 m 的元素数量
+    for (int i = 1, j = 0, cnt = 0; i <= n; i++)
     {
-        // 指针 j 向右移动，直到区间和 sum >= S 或者 j 超出范围
-        while (i > j || (j + 1 <= n && sum < S))
+        // 移动 j 指针，直到当前子串中有至少 k 个元素大于等于 m
+        while (i > j || (j + 1 <= n && cnt < k))
         {
-            j++;           // 右指针右移
-            sum += a[j];   // 累加区间和
+            j++;
+            cnt += (a[j] >= m);   // 如果 a[j] >= m，则 cnt 增加
         }
 
-        // 如果当前区间和 sum >= S，更新 ans 为当前区间的长度和 ans 中的较小值
-        if (sum >= S)
+        // 如果满足条件，则计算从当前子串开始到数组末尾的所有可能的子串数量
+        if (cnt >= k)
         {
-            ans = min(ans, j - i + 1);
+            ans += n - j + 1;
         }
 
-        // 移动左指针 i，并从 sum 中减去 a[i]，以便继续寻找更短的区间
-        sum -= a[i];
+        // 移动 i 指针，减少 cnt 中相应的计数
+        cnt -= (a[i] >= m);
     }
 
-    // 如果 ans 仍然大于 n，表示没有找到满足条件的区间，输出 0；否则输出 ans
-    cout << (ans > n ? 0 : ans) << '\n';
-
-    return 0; 
+    cout << ans << '\n';
+    return 0;
 }
