6681688857adef1f30b696e377cf8b41e720e620
jojo--1 <1468608544@qq.com>
2025-02-26 14:49:34 +0800

    C++：（3423）安全序列（dp），在一条直线上放置若干个油桶，直线上有 n 个空位。每两个油桶之间必须至少有 k 个空位隔开。计算有多少种不同的放置油桶的方案

--- Sandy/Sandy/sandy.cpp
+++ Sandy/Sandy/sandy.cpp
@@ -1,35 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<iostream>
-#include<algorithm>
+#include <iostream>
 using namespace std;
-using ll = long long;
-const int N = 105;      // 数字三角形的最大行数
-ll a[N][N], dp[N][N];   // a: 存储数字三角形；dp: 存储从当前位置到底部的最大路径和
+using ll = long long;  
+const ll N = 1e6 + 9, p = 1e9 + 7;  // N 是数组大小，p 是模数
+ll dp[N], prefix[N];  
 
 int main()
 {
-	int n; cin >> n;   
+    int n, k;
+    cin >> n >> k;  // n 是空位数，k 是油桶之间需要的最小空位数
 
-	// 输入数字三角形
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= i; j++)
-		{
-			cin >> a[i][j];
-		}
-	}
+    // dp[0] 和 prefix[0] 初始化为 1，表示没有空位时的方案数为 1
+    // dp[i] 表示以位置 i 结尾的方案总数
+    dp[0] = prefix[0] = 1;  
 
-	// 从底部向上计算最大路径和
-	for (int i = n; i >= 1; i--) // 从最后一行开始向上遍历
-	{
-		// 遍历当前行的每个元素
-		for (int j = 1; j <= i; j++) 
-		{
-			// 当前元素的值加上下一行相邻两个元素的最大值
-			dp[i][j] = a[i][j] + max(dp[i + 1][j], dp[i + 1][j + 1]);
-		}
-	}
+    // 用 dp 计算每个位置的方案数
+    for (int i = 1; i <= n; i++)
+    {
+        // 如果当前位置 i 减去 k - 1 小于 0，说明无法放置油桶，方案数为 1
+        if (i - k - 1 < 0)
+        {
+            dp[i] = 1;
+        }
+        else
+        {
+            // prefix[i - k - 1] 表示在 i - k - 1 位置之前的方案数
+            dp[i] = prefix[i - k - 1];
+        }
+           
+        // 更新 prefix 数组
+        prefix[i] = (prefix[i - 1] + dp[i]) % p;  
+    }
 
-	cout << dp[1][1] << '\n';
-	return 0;
-}      
+    cout << prefix[n] << '\n';
+    return 0;
+}
