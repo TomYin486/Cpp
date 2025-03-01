736c103a8c9781373970a90ea566cc350b328870
jojo--1 <1468608544@qq.com>
2025-03-01 22:07:17 +0800

    C++：（20）有效的括号（Stack），判断一个仅由括号 '() [] {}' 组成的字符串 s 是否是有效的，1.左括号必须用相同类型的右括号闭合，2.左括号必须以正确的顺序闭合，3.每个右括号都有一个对应的相同类型的左括号

--- Sandy/Sandy/sandy.cpp
+++ Sandy/Sandy/sandy.cpp
@@ -1,37 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 using namespace std;
-using ll = long long;  
-const ll N = 1e6 + 9, p = 1e9 + 7;  // N 是数组大小，p 是模数
-ll dp[N], prefix[N];  
 
-int main()
-{
-    int n, k;
-    cin >> n >> k;  // n 是空位数，k 是油桶之间需要的最小空位数
-
-    // dp[0] 和 prefix[0] 初始化为 1，表示没有空位时的方案数为 1
-    // dp[i] 表示以位置 i 结尾的方案总数
-    dp[0] = prefix[0] = 1;  
-
-    // 用 dp 计算每个位置的方案数
-    for (int i = 1; i <= n; i++)
-    {
-        // 如果当前位置 i 减去 k - 1 小于 0，说明无法放置油桶，方案数为 1
-        if (i - k - 1 < 0)
-        {
-            dp[i] = 1;
+class Solution {
+public:
+    bool isValid(string s) {
+        // 左括号入栈，右括号出栈
+        stack<char> stk;
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') stk.push(ch);
+            else {
+                if (stk.empty()) {
+                    return false;  // 没有匹配的左括号
+                }
+                char top = stk.top();
+                if ((ch == ')' && top == '(') ||
+                    (ch == '}' && top == '{') ||
+                    (ch == ']' && top == '[')) {
+                    stk.pop();
+                }
+                else return false;  // 括号不匹配
+            }
         }
-        else
-        {
-            // prefix[i - k - 1] 表示在 i - k - 1 位置之前的方案数
-            dp[i] = prefix[i - k - 1];
-        }
-           
-        // 更新 prefix 数组
-        prefix[i] = (prefix[i - 1] + dp[i]) % p;  
+        return stk.empty();  // 如果栈为空，说明所有括号都匹配
     }
-
-    cout << prefix[n] << '\n';
-    return 0;
-}
+};
