a0381bf088dd4c60515e9d4d6e556047fed708f0
jojo--1 <1468608544@qq.com>
2025-03-07 23:22:01 +0800

    C++：（2331）计算布尔二叉树的值，计算一棵完整二叉树的根节点的布尔值

--- Sandy/Sandy/sandy.cpp
+++ Sandy/Sandy/sandy.cpp
@@ -4,32 +4,17 @@ using namespace std;
 
 class Solution {
 public:
-	int tribonacci(int n) {
-		// 1.创建 dp 表
-		// 2.初始化
-		// 3.填表
-		// 4.返回值
+	bool evaluateTree(TreeNode* root) {
+		// 如果当前节点是叶子节点（没有左孩子），直接返回其值对应的布尔值
+		if (root->left == nullptr) return root->val == 0 ? false : true;
 
-		// 处理边界情况
-		if (n == 0) return 0;
-		if (n == 1 || n == 2) return 1;
+		// 计算左子树的值
+		bool left = evaluateTree(root->left);
 
-		vector<int> dp(n + 1);
-		dp[0] = 0, dp[1] = dp[2] = 1;
-		for (int i = 3; i <= n; i++)
-			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		// 计算右子树的值
+		bool right = evaluateTree(root->right);
 
-		// 方法二
-		/*int a = 0, b = 1, c = 1, d = 0;
-		for (int i = 3; i <= n; i++)
-		{
-			// T(n) = T(n-3) + T(n-2) + T(n-1)
-			d = a + b + c;
-			a = b;
-			b = c;
-			c = d;
-		}
-		return d;*/
-		return dp[n];
+		// 根据当前节点的值决定进行 | 还是 & 操作
+		return root->val == 2 ? left | right : left & right;
 	}
 };
