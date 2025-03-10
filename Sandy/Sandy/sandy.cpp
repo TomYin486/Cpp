d7e67dcf299ab4a89dd4eb6346d8f558201375bb
jojo--1 <1468608544@qq.com>
2025-03-10 22:48:13 +0800

    C++：（364）跳石头（二分），在一条笔直的河道中，起点和终点之间分布着一些岩石。选手需要从起点出发，依次跳跃到相邻的岩石，最终到达终点。组委会计划最多只能移走 M 块岩石（不能移走起点和终点的岩石），使得选手在比赛过程中的最短跳跃距离尽可能长，输出最短 跳跃距离的最大值

--- Sandy/Sandy/sandy.cpp
+++ Sandy/Sandy/sandy.cpp
@@ -1,20 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Solution {
-public:
-	bool evaluateTree(TreeNode* root) {
-		// 如果当前节点是叶子节点（没有左孩子），直接返回其值对应的布尔值
-		if (root->left == nullptr) return root->val == 0 ? false : true;
+// L: 起点到终点的距离，n: 岩石数量，m: 最多可以移走的岩石数量
+int L, n, m; 
+const int N = 1e5; 
+int stone[N];   // 每块岩石的位置
 
-		// 计算左子树的值
-		bool left = evaluateTree(root->left);
+// 判断是否可以在移走不超过 m 块岩石的情况下，使得最短跳跃距离至少为 step
+bool check(int step)
+{
+    int pos = 0; 
+    int cnt = 0;  // 需要移走的岩石数量
+    for (int i = 1; i <= n + 1; i++) 
+    {
+        // 如果当前岩石与上一个岩石的距离小于 step，需要移走这块岩石
+        if (stone[i] - stone[pos] < step) cnt++;
+        else pos = i;  // 跳到这块岩石
+    }
 
-		// 计算右子树的值
-		bool right = evaluateTree(root->right);
+    // 移走的岩石数量超过 m
+    if (cnt > m) return false; 
+    return true; 
+}
 
-		// 根据当前节点的值决定进行 | 还是 & 操作
-		return root->val == 2 ? left | right : left & right;
-	}
-};
+int main()
+{
+    cin >> L >> n >> m; 
+    stone[n + 1] = L;    // 将终点的位置存入岩石数组的最后一个位置
+    for (int i = 1; i <= n; i++)  cin >> stone[i];  
+        
+    int left, right; 
+    left = 0, right = L; 
+    // 二分查找
+    while (left < right) 
+    {
+        int mid = 0;
+        mid = left + (right - left + 1) / 2; 
+        if (check(mid)) left = mid;  // 尝试更大的值
+        else right = mid - 1;  // 尝试更小的值
+    }
+
+    cout << left << '\n'; 
+    return 0;
+}
