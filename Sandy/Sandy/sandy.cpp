57b7a67557f6f4cb5f197bf35af7d40edc908305
jojo--1 <1468608544@qq.com>
2025-03-14 23:20:34 +0800

    C++：（389）摆花（dp），花店门口摆放一排花，总共有 m 盆花。现列出了顾客最喜欢的 n 种花，每种花有一个编号（从 1 到 n）。为了满足顾客的喜好，规定第 i 种花 的摆放数量不能超过 aᵢ 盆，同一种花必须放在一起且不同种类的花必须按照编号从小到大的顺序依次摆放 ，计算一共有多少种不同的摆花方案

--- Sandy/Sandy/sandy.cpp
+++ Sandy/Sandy/sandy.cpp
@@ -1,46 +1,31 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 using namespace std;
+using ll = long long;  
+const int N = 105;        
+const ll p = 1e6 + 7;  
 
-// L: 起点到终点的距离，n: 岩石数量，m: 最多可以移走的岩石数量
-int L, n, m; 
-const int N = 1e5; 
-int stone[N];   // 每块岩石的位置
-
-// 判断是否可以在移走不超过 m 块岩石的情况下，使得最短跳跃距离至少为 step
-bool check(int step)
+ll a[N], dp[N][N];    // dp[i][j] 表示使用前 i 种花，摆放 j 盆花的方案数
+int main() 
 {
-    int pos = 0; 
-    int cnt = 0;  // 需要移走的岩石数量
-    for (int i = 1; i <= n + 1; i++) 
-    {
-        // 如果当前岩石与上一个岩石的距离小于 step，需要移走这块岩石
-        if (stone[i] - stone[pos] < step) cnt++;
-        else pos = i;  // 跳到这块岩石
+    int n, m; cin >> n >> m;    // n 为花种数，m 为总盆数
+    
+    for (int i = 1; i <= n; i++) cin >> a[i];  
+
+    // 没有花时，摆放 0 盆花的方案数为 1
+    dp[0][0] = 1;  
+
+    // 计算每种花的摆放方案数
+    for (int i = 1; i <= n; i++) 
+    {  // 遍历每种花
+        for (int j = 0; j <= m; j++) 
+        {  // 遍历摆放的盆数
+            for (int k = 0; k <= a[i] && k <= j; k++) 
+            {   // 计算前花可以摆放的盆数
+                dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % p;  
+            }
+        }
     }
 
-    // 移走的岩石数量超过 m
-    if (cnt > m) return false; 
-    return true; 
-}
-
-int main()
-{
-    cin >> L >> n >> m; 
-    stone[n + 1] = L;    // 将终点的位置存入岩石数组的最后一个位置
-    for (int i = 1; i <= n; i++)  cin >> stone[i];  
-        
-    int left, right; 
-    left = 0, right = L; 
-    // 二分查找
-    while (left < right) 
-    {
-        int mid = 0;
-        mid = left + (right - left + 1) / 2; 
-        if (check(mid)) left = mid;  // 尝试更大的值
-        else right = mid - 1;  // 尝试更小的值
-    }
-
-    cout << left << '\n'; 
+    cout << dp[n][m] << '\n';
     return 0;
 }
