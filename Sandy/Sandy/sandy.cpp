bc42f11a103dfa507a26752919f67327ca658cd7
jojo--1 <1468608544@qq.com>
2025-03-15 22:15:45 +0800

    C++：（3891）帮派弟位（dfs），小明在一个帮派中，帮派的关系网是一棵树，每个帮众有一个唯一的序号。给定帮派的总人数 n 和小明的序号 m，以及 n - 1 条附属关系（表示树的结构），计算小明在帮派中的地位排名（按照每个帮众的手下数量从大到小排序，如果手下数量相 同，则按序号从小到大排序）

--- Sandy/Sandy/sandy.cpp
+++ Sandy/Sandy/sandy.cpp
@@ -1,31 +1,59 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-using ll = long long;  
-const int N = 105;        
-const ll p = 1e6 + 7;  
+const int N = 1e5 + 9;
+vector<int> g[N];     // 邻接表，存储帮派的关系
+int siz[N];           // siz[x] 表示以 x 为根节点的子树的大小，相当于 x 的手下数量
 
-ll a[N], dp[N][N];    // dp[i][j] 表示使用前 i 种花，摆放 j 盆花的方案数
-int main() 
+struct Node
 {
-    int n, m; cin >> n >> m;    // n 为花种数，m 为总盆数
-    
-    for (int i = 1; i <= n; i++) cin >> a[i];  
+    int siz, id;   // siz 表示手下数量，id 表示帮众的序号
 
-    // 没有花时，摆放 0 盆花的方案数为 1
-    dp[0][0] = 1;  
+    // 排序
+    bool operator<(const Node& u) const
+    {
+        return siz == u.siz ? id < u.id : siz > u.siz;
+    }
+};
 
-    // 计算每种花的摆放方案数
-    for (int i = 1; i <= n; i++) 
-    {  // 遍历每种花
-        for (int j = 0; j <= m; j++) 
-        {  // 遍历摆放的盆数
-            for (int k = 0; k <= a[i] && k <= j; k++) 
-            {   // 计算前花可以摆放的盆数
-                dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % p;  
-            }
-        }
+vector<Node> v;    // 存储所有帮众的信息
+
+// 计算以 x 为根节点的子树大小(手下数量)
+void dfs(int x, int p)
+{
+    siz[x] = 1;        // 初始化当前节点的手下数量为 1（自己）
+    for (const auto& y : g[x])  // 遍历 x 的所有子节点
+    {
+        if (y == p) continue;  // 跳过父节点
+        dfs(y, x);             // 计算子节点的手下数量，当前 y 的父节点为 x 
+        siz[x] += siz[y];
+    }
+}
+
+int main()
+{
+    int n, m; cin >> n >> m;  // 输入帮派总人数 n 和小明的序号 m
+
+    // 输入帮派的关系网
+    for (int i = 1; i < n; ++i)
+    {
+        int x, y; cin >> x >> y;
+        g[y].push_back(x);  // 将 x 添加到 y 的子节点中
     }
 
-    cout << dp[n][m] << '\n';
+    // 从根节点 1 开始，计算每个节点的手下数量
+    dfs(1, 0);
+
+    // 将所有帮众的信息存储到数组 v 中
+    for (int i = 1; i <= n; ++i)
+        v.push_back({ siz[i] - 1, i });  // siz[i] - 1 表示除去自己后的手下数量
+
+    // 按照手下数量从大到小排序，如果手下数量相同则按序号从小到大排序
+    sort(v.begin(), v.end());
+
+    // 遍历排序后的结果，找到小明的排名
+    for (int i = 0; i < v.size(); ++i)
+        if (v[i].id == m)
+            cout << i + 1 << '\n';  // 输出排名
+
     return 0;
 }
